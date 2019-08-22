195af5df159ec33b23b7eac457dc0261b778c909
noriya <noriya.saeki@gmail.com>
2019-08-22 22:09:39 +0900

    エスケープする関数作成

--- common.php
+++ common.php
@@ -1,3 +1,23 @@
 <?php
+function gengo($seireki) {
+    if (1868 < $seireki && $seireki <= 1911) {
+        $gengo = '明治';
+    } else if (1911 < $seireki && $seireki <= 1925) {
+        $gengo = '大正';
+    } else if (1926 <= $seireki && $seireki <= 1988) {
+        $gengo = '昭和';
+    } else if (1989 <= $seireki && $seireki <= 2019) {
+        $gengo = '平成';
+    } else {
+        $gengo = '令和';
+    }
+    return($gengo);
+}
 
+function sanitize($before){
+    foreach($before as $key => $value){
+        $after[$key] = $htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+    return $after;
+}
 ?>
